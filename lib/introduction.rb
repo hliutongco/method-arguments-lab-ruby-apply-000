7f4bdb707b8f98872a6054742b8c69c6b110582e
Helen Liutongco <bellpickle@gmail.com>
2017-12-26 21:04:05 +0000

    Done.

---
+++ lib/introduction.rb
@@ -0,0 +1,13 @@
+#The introduction method takes in the name argument
+#It puts out a string that includes the name argument through string interpolation
+
+def introduction(name)
+  puts "Hi, my name is #{name}."
+end
+
+#The introduction_with_language method takes in the name and language arguments
+#It puts out a string that includes both arguments through string interpolation
+
+def introduction_with_language(name, language)
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
+end
